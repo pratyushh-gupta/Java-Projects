dccdf2b72a0229b650f32659da1408d43837459a
Prayush Gupta <pratyush.gupta.in@gmail.com>
2026-07-10 19:05:46 +0530

    Update Student.java

---
+++ src/Student.java
@@ -0,0 +1,15 @@
+class Student{
+  int id;
+  String name;
+  int age;
+  char grade;
+  String contact;
+
+  public Student(int id, String name, int age, char grade, String contact){
+    this.id = id;
+    this.name = name; 
+    this.age = age; 
+    this.grade = grade;
+    this.contact = contact;
+  }
+}
