0a411b360f129fc5b24215624515b884f0baa966
Prayush Gupta <pratyush.gupta.in@gmail.com>
2026-07-17 14:14:45 +0530

    created the variables

---
+++ Control Flow & Arrays/src/GradeManagementSystem.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class GradeManagementSystem{
+  static final int MAX_STUDENTS = 100;
+  static final int SUBJECTS = 5;
+  static String[] studentNames = new String[MAX_STUDENTS];
+  static double[][] studentMarks = new double[MAX_STUDENTS][SUBJECTS];
+  static int studentCount = 0;
+  static Scanner sc = new Scanner(System.in);
+  public static void main(String args[]){
+    
+  }
+}
