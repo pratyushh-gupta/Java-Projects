6513091b29f42247ce4043b4f7e4438f5f74d0d6
Prayush Gupta <pratyush.gupta.in@gmail.com>
2026-07-17 14:21:07 +0530

    created the menu

--- Control Flow & Arrays/src/GradeManagementSystem.java
+++ Control Flow & Arrays/src/GradeManagementSystem.java
@@ -8,6 +8,25 @@ public class GradeManagementSystem{
   static int studentCount = 0;
   static Scanner sc = new Scanner(System.in);
   public static void main(String args[]){
-    
+    while(true){
+      System.out.println("\n==== GRADE MANAGEMENT SYSTEM ====");
+      System.out.println("1. Add Student");
+      System.out.println("2. Exit");
+
+      System.out.println("Enter choice: ");
+      int choice = sc.nextInt();
+      sc.nextLine();
+
+      switch(choice){
+        case 1:
+          addStudent();
+          break;
+        case 2:
+          System.out.println("Thank You!");
+          System.exit(0);
+        default:
+          System.out.println("Invalid Choice!");
+      }
+    }
   }
 }
